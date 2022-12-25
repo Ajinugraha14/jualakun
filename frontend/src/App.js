27adb29625d3b48e7af5e9be2b4b49fbdd6c36b1
Ajinugraha14 <zieaji55@gmail.com>
2022-12-25 15:15:20 +0700

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Adera Store</a>
+      </header>
+      <main>List product</main>
+    </div>
+  );
+}
+
+export default App;
